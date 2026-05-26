a7afe5399e1ec6a2d2718afd1be6654aa73b5c17
Joao Cheab <joaopaulocheab@gmail.com>
2026-05-26 01:26:21 -0300

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -25,7 +26,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Inter:wght@300;400;500;600&family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="bg-beige-light text-dark font-body">{children}<SpeedInsights /></body>
+      <body className="bg-beige-light text-dark font-body">{children}<Analytics /><SpeedInsights /></body>
     </html>
   )
 }
